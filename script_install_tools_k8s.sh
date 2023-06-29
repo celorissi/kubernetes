d745db050f7d7b710ee8b180bb3b6720eadef0fb
celorissi <celo.rissi@gmail.com>
2023-06-29 21:25:05 +0000

    update 26/06/23

--- script_install_tools_k8s.sh
+++ script_install_tools_k8s.sh
@@ -28,6 +28,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+systemctl start docker
+
 sudo apt update
 
 apt-cache policy docker-ce
@@ -37,6 +39,8 @@ sudo apt install docker-ce
 sudo systemctl start docker
 
 #sudo systemctl status docker
+
+gpasswd -a $USER docker
 
 sudo usermod -aG docker ${USER}
 
